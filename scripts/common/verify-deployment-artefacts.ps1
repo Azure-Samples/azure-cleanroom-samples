208c1416fbe990a58b56dedc908259ec99be25d9
Anoop R Desai <anrdesai@microsoft.com>
2024-11-30 01:03:00 +0530

    Remove dependency on gh login

--- scripts/common/verify-deployment-artefacts.ps1
+++ scripts/common/verify-deployment-artefacts.ps1
@@ -3,7 +3,10 @@ function Assert-CleanroomAttestation {
         [Parameter(Mandatory = $true)]
         [string[]]$containerImages,
 
-        [string]$containerTag = "2.0.0"
+        [string]$containerTag = "2.0.0",
+
+        [string]$samplesRoot = "/home/samples",
+        [string]$privateDir = "$samplesRoot/demo-resources/private"
     )
     #https://learn.microsoft.com/en-us/powershell/scripting/learn/experimental-features?view=powershell-7.4#psnativecommanderroractionpreference
     $ErrorActionPreference = 'Stop'
@@ -13,19 +16,43 @@ function Assert-CleanroomAttestation {
     $workflowPath = ".github/workflows/release.yml"
     $workflowRef = "refs/heads/main"
 
-    Write-Log Verbose `
-        "Logging in to GitHub to verify container image attestations..."
-    gh auth login --web
-
     # Fetch the commit ID corresponding to the container versions
-    $tagDetails = gh api "repos/$repo/tags" | ConvertFrom-Json | Where-Object {$_.name -eq $containerTag}
+    $tagDetails = curl -L "https://api.github.com/repos/$repo/tags" | ConvertFrom-Json | Where-Object {$_.name -eq $containerTag}
     $commitId = $tagDetails.commit.sha
+
+    # Verify attestations for container images offline:
+    # https://docs.github.com/en/actions/security-for-github-actions/using-artifact-attestations/verifying-attestations-offline
+    gh attestation trusted-root > "$privateDir/trusted_root.jsonl"
+    if ($LASTEXITCODE -ne 0) {
+        Write-Log Error "Failed to fetch trusted root."
+        exit 1
+    }
   
     foreach ($image in $containerImages) {
         Write-Log Verbose "Verifying image '$image'..."
-        $attestationResult = gh attestation verify `
+        $digest = $image.Split("@")[1]
+        rm -f "$privateDir/$digest.jsonl"
+
+        # Fetch attestation from GitHub API.
+        $attestationResult = curl -L "https://api.github.com/repos/$repo/attestations/$digest" | ConvertFrom-Json
+        if ($attestationResult.Length -ne 1) {
+            Write-Log Error "Invalid attestation result for image '$image'"
+            exit 1
+        }
+
+        # Convert the attestation to JSONL format, required for offline verification:
+        # https://docs.github.com/en/actions/security-for-github-actions/using-artifact-attestations/verifying-attestations-offline
+        $attestationResult = $attestationResult[0]
+        foreach ($attestation in $attestationResult.attestations) {
+            $attestation.bundle | ConvertTo-Json -Depth 100 -Compress | Out-File "$privateDir/$digest.jsonl" -Append
+        }
+
+        $attestationVerificationResult = gh attestation verify `
             "oci://$image" `
-            --repo $repo --format json | ConvertFrom-Json
+            --repo $repo `
+            --bundle "$privateDir/$digest.jsonl" `
+            --custom-trusted-root "$privateDir/trusted_root.jsonl" `
+            --format json | ConvertFrom-Json
         
         if ($LASTEXITCODE -ne 0) {
             Write-Log Error "Failed to verify image '$image'."
@@ -33,7 +60,7 @@ function Assert-CleanroomAttestation {
         }
 
         $imagePath = $image.Split("@")[0]
-        $attestation = $attestationResult | Where-Object {$_.verificationResult.statement.subject.name -eq $imagePath}
+        $attestation = $attestationVerificationResult | Where-Object {$_.verificationResult.statement.subject.name -eq $imagePath}
 
         if ($attestation.verificationResult.statement.predicate.buildDefinition.resolvedDependencies.digest.gitCommit -ne $commitId) {
             Write-Log Error "Images in the deployment template are not from the latest commit."
